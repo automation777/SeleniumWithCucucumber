a25ec87289640968fb6e9858966ed3f598076697
automation777 <31100666+automation777@users.noreply.github.com>
2018-04-17 18:40:44 +0530

    updated the chromedriver.exe file

--- src/test/java/steps/Hook.java
+++ src/test/java/steps/Hook.java
@@ -29,7 +29,7 @@ public class Hook extends BaseUtil{
 
 
         //Chrome driver
-        System.setProperty("webdriver.chrome.driver", "C:\\Libs\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "/opt/SeleniumWithCucucumber.exe");
         base.Driver = new ChromeDriver();
     }
 
